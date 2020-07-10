da2df8be3886fd666c23796859811eebd8c4e59d
kyloris0660 <1489821378@qq.com>
2020-07-10 18:58:06 +0800

    证书部分.

--- main.sh
+++ main.sh
@@ -18,6 +18,48 @@ red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
 
+function configure_nginx(){
+cat > /etc/nginx/nginx.conf <<-EOF
+user  root;
+worker_processes  1;
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+events {
+    worker_connections  1024;
+}
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+    access_log  /var/log/nginx/access.log  main;
+    sendfile        on;
+    #tcp_nopush     on;
+    keepalive_timeout  120;
+    client_max_body_size 20m;
+    #gzip  on;
+    server {
+        listen       80;
+        server_name  $your_domain;
+        root /usr/share/nginx/html;
+        index index.php index.html index.htm;
+    }
+}
+EOF
+systemctl restart nginx.service
+mkdir /usr/src/trojan-cert
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/
+    ~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
+    --key-file   /usr/src/trojan-cert/private.key \
+    --fullchain-file /usr/src/trojan-cert/fullchain.cer \
+    --reloadcmd  "systemctl force-reload  nginx.service"
+if test -s /usr/src/trojan-cert/fullchain.cer; then
+    green "Certificate OK."
+    green "You should be able to visit https://${domin}:443"
+}
+
 function install_trojain(){
 if cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
@@ -39,5 +81,18 @@ fi
 green "Installing dependences."
 apt-get -y install nginx wget curl tar >/dev/null 2>&1
 systemctl enable nginx.service
-
+yellow "Enter your domin: "
+read domin
+real_addr=`ping ${domin} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+local_addr=`curl ipv4.icanhazip.com`
+if [ $real_addr == $local_addr ] ; then
+    green "Domin OK."
+    green "IP:$real_addr"
+    green "Configuring nginx and http service."
+else
+    red "Domain name resolution does not match ip or resolution failed."
+    red "Check Domin."
+    exit
+fi
+configure_nginx
 }
